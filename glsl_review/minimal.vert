4fb5dbf04caeb63f3322fd2127e01f42f55fd182
Wonil <red112@naver.com>
2023-01-07 14:40:41 +0900

    [opengl].[#2.GLSL] 04. gl_Color 끄적거리기

--- glsl_review/minimal.vert
+++ glsl_review/minimal.vert
@@ -1,9 +1,16 @@
 // minimal vertex shader
 // www.lighthouse3d.com
 void main()
-{
-	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;	
+{	
+	//vec4 invColor;
+	//invColor[0]=1.f-gl_Color[0];
+	//invColor[1]=1.f-gl_Color[1];
+	//invColor[2]=1.f-gl_Color[2];
+	//invColor[3]=1.f-gl_Color[3];
+	//gl_FrontColor = invColor;
+
 	gl_FrontColor = gl_Color;
+	gl_Position = ftransform();
 }
 
 
--- glsl_review/wingl_review.cpp
+++ glsl_review/wingl_review.cpp
@@ -93,11 +93,18 @@ void display()
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//Draw
-	glPushMatrix();
-	glRotatef(rotate_angle, 0.f, 1.f, 0.f);
-	glutSolidTeapot(0.5);
-	glPopMatrix();
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.f, 0.f, 0.f);
+	glVertex3f(-0.5, -0.5, 0.0);
+	glColor3f(0.f, 1.f, 0.f);
+	glVertex3f(0.5, 0.0, 0.0);
+	glColor3f(0.f, 0.f, 1.f);
+	glVertex3f(0.0, 0.5, 0.0);
+	glEnd();
 	glFlush();
+
+
+
 	glutSwapBuffers();
 	rotate_angle = rotate_angle + 0.1f;
 	if (rotate_angle > 360.f) rotate_angle -= 360.f;
